e67bb20b527d59e6f4e3c87f7901480965c7d016
ReemAlattas <ralattas@live.com>
2016-08-07 18:00:34 -0400

    add createCylinder func

--- RagdollDemo.h
+++ RagdollDemo.h
@@ -70,6 +70,9 @@ public:
 	virtual void keyboardCallback(unsigned char key, int x, int y);
     
     void CreateBox(int index, double x, double y, double z, double length, double width, double height);
+    
+    void CreateCylinder(int index, double x, double y, double z, double length, double width, double height);
+
 
 	static DemoApplication* Create()
 	{
